cdd01e00b8ff92ce343c668cd31ddd99b672eacf
FrancoEMTG <aesevv@gmail.com>
2023-08-03 19:42:20 -0300

    añadiendo variables y concepto general

---
+++ app.js
@@ -0,0 +1,15 @@
+// Ingresar texto
+// devolver las palabras repetidas de mayor a menor
+
+
+let texto = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet aspernatur eius repellat ipsa expedita voluptatibus fugiat doloremque totam veniam, possimus rem maxime. Maiores, natus! Dignissimos quaerat commodi nisi neque molestias!Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet aspernatur eius repellat ipsa expedita voluptatibus fugiat doloremque totam veniam, possimus rem maxime. Maiores, natus! Dignissimos quaerat commodi nisi neque molestias!"
+
+let texto_hecho_en_array = texto.split(" ")
+
+let acumulador = []
+
+for (let i = 0; i < texto_hecho_en_array.length; i++) {
+    if (texto_hecho_en_array[0] = texto_hecho_en_array[0+1]) {
+        
+    }
+}
